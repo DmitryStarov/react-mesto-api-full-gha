767e8b7aa4e1380849815f67e85802333e3b64ed
DmitryStarov <314263@bk.ru>
2023-08-01 02:30:52 +0800

    refactoring/base url

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,11 +1,11 @@
 export const connectionSettings = {
-  baseUrl: "https://api.starov.nomoreparties.co",
+  baseUrl: "api.starov.nomoreparties.co",
   headers: {
     "Authorization": `Bearer ${localStorage.getItem('jwt')}`,
     "Content-Type": "application/json",
   },
 };
 export const authSettings = {
-  baseUrl: "https://api.starov.nomoreparties.co",
+  baseUrl: "api.starov.nomoreparties.co",
   headers: { "Content-Type": "application/json" },
 };
