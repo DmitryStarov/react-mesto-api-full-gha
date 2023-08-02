1fc1d1d2de19c960d4b936ead75b221d1cc9ee44
DmitryStarov <314263@bk.ru>
2023-08-02 16:46:41 +0800

    refactoring/cors

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,7 @@ app.use(limiter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(requestLogger);
-app.use(cors);
+app.use(cors());
 //краш-тест
 app.get('/crash-test', () => {
   setTimeout(() => {
